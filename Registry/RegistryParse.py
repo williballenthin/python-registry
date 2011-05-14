30a309e217b8948b6925d5ddb678f486d4371ee5
Willi Ballenthin <willi.ballenthin@gmail.com>
2011-05-14 11:45:56 -0400

    changed name of python-registry in licenses

--- Registry/RegistryParse.py
+++ Registry/RegistryParse.py
@@ -1,20 +1,19 @@
 #!/bin/python
 
-#    This file is part of Python Registry.
+#    This file is part of python-registry.
 #
-#    Python Registry is free software: you can redistribute it and/or modify
+#    python-registry is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    Python Registry is distributed in the hope that it will be useful,
+#    python-registry is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with Python Registry.  If not, see <http://www.gnu.org/licenses/>.
-
+#    along with python-registry.  If not, see <http://www.gnu.org/licenses/>.
 
 import struct
 from datetime import datetime
