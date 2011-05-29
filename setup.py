c85d3b3efb011eabd7c3da714f5e9bd4609bf881
Willi Ballenthin <willi.ballenthin@gmail.com>
2011-05-29 12:56:08 -0400

    changes to package for PyPi release

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='Registry',
-      version='0.1',
+setup(name='python-registry',
+      version='0.1.1',
       description='Read access to Windows Registry files.',
       author='Willi Ballenthin',
       author_email='willi.ballenthin@gmail.com',
-      url='http://www.williballenthin.com/registry',
+      url='http://www.williballenthin.com/registry/',
       packages=['Registry'],
      )
