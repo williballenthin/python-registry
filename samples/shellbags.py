ff47780c94f9cf6692d6a357fd8982f5f8291b01
Willi Ballenthin <willi.ballenthin@gmail.com>
2011-06-12 21:42:54 -0400

    Fix parsing of shellbag strings.
    Fixed parsing of some shellbag path strings where UTF16 characters were getting truncated. Also, fix to make sure we are actually getting Python strings, and not some lucky binary that looks good.

--- samples/shellbags.py
+++ samples/shellbags.py
@@ -6,8 +6,8 @@ from Registry import Registry
 
 SB_MRU_TYPE_PREDEFINED = 0x1F
 SB_MRU_TYPE_ARCHIVE = 0x32
-SB_MRU_TYPE_NAMEEARLY = 0x2E
-SB_MRU_TYPE_NAMEVERYEARLY = 0x2F
+SB_MRU_TYPE_UNICODEONLY = 0x2E
+SB_MRU_TYPE_NAMEIMMEDIATE = 0x2F
 
 def dosdate(dosdate, dostime):
     try:
@@ -30,22 +30,38 @@ def dosdate(dosdate, dostime):
         return datetime.datetime.min
 
 def get_shellbags(registry):
-    keys = {}
     shellbags = []
-    windows = registry.open("Software\\Microsoft\\Windows\\ShellNoRoam")
+    try:
+        windows = registry.open("Software\\Microsoft\\Windows\\ShellNoRoam")
+    except Registry.RegistryKeyNotFoundException:
+        try:
+            windows = registry.open("Local Settings\\Software\\Microsoft\\Windows\\Shell")
+        except Registry.RegistryKeyNotFoundException:
+            print "Unable to find shellbag key."
+            sys.exit(-1)
     bagmru = windows.subkey("BagMRU")
-#    bags = windows.subkey("Bags")
 
     def shellbag_rec(key, bag_prefix, path_prefix):
+        """
+        `key`: The current 'BagsMRU' key to recurse into.
+        `bag_prefix`: A string containing the current subkey path of the relevant 'Bags' key.
+            It will look something like '1\\2\\3\\4'.
+        `path_prefix` A string containing the current human-readable, file system path so far constructed.
+        """
         for value in key.values():
             if not re.match("\d+", value.name()):
                 continue
             mru_type = ord(value.value()[2])
 
+            # default timestamps
+            mtime = datetime.datetime.min
+            atime = datetime.datetime.min
+            ctime = datetime.datetime.min
+
             # get ASCII name at known offset
-            if mru_type == SB_MRU_TYPE_NAMEEARLY:
+            if mru_type == SB_MRU_TYPE_UNICODEONLY:
                 nameAStart = 0xA
-            elif mru_type == SB_MRU_TYPE_NAMEVERYEARLY:
+            elif mru_type == SB_MRU_TYPE_NAMEIMMEDIATE:
                 nameAStart = 0x3
             else:
                 nameAStart = 0xE
@@ -53,20 +69,20 @@ def get_shellbags(registry):
 
             # next, get the full unicode version
             if mru_type == SB_MRU_TYPE_PREDEFINED:
-                nameW = "??"
-                mtime = datetime.datetime.min
-                atime = datetime.datetime.min
-                ctime = datetime.datetime.min
-            elif mru_type == SB_MRU_TYPE_NAMEEARLY:
-                nameW = value.value()[0xA:].partition(b"\x00\x00")[0]
-                mtime = datetime.datetime.min
-                atime = datetime.datetime.min
-                ctime = datetime.datetime.min
-            elif mru_type == SB_MRU_TYPE_NAMEVERYEARLY:
-                nameW = nameA.encode("utf16", "ignore")[2:] # ignore leading 2 bytes (byte ordering)
-                mtime = datetime.datetime.min
-                atime = datetime.datetime.min
-                ctime = datetime.datetime.min
+#                nameW = "??"
+                nameW = "[" + bag_prefix + "|" + value.name() + "]"
+            elif mru_type == SB_MRU_TYPE_UNICODEONLY:
+                # the finds the end of the utf16 string in the binary
+                # we know it must end in 00 00, but it could end in 00 00 00
+                # if the last character is ASCII. We dont want to truncate 
+                # the last character in that case
+                bin = value.value()[0xA:]
+                end_idx = bin.find(b"\x00\x00")
+                if end_idx % 2 == 1:
+                    end_idx += 1
+                nameW = bin[:end_idx].decode("utf16")
+            elif mru_type == SB_MRU_TYPE_NAMEIMMEDIATE:
+                nameW = nameA
             else:
 #                needle = nameA.lower().partition("~")[0].encode("utf16")[2:]
 #                wide_start = value.value().lower().find(needle)
@@ -106,7 +122,10 @@ def get_shellbags(registry):
 #    print "MTIME, ATIME, CTIME, PATH"
     for shellbag in shellbags:
 #        print "%s, %s, %s, %s" % (shellbag["mtime"], shellbag["atime"], shellbag["ctime"], shellbag["path"])
-       print "%s" % (shellbag["path"])
+        try:
+            print "%s" % (shellbag["path"])
+        except UnicodeEncodeError:
+            print list(shellbag["path"])
 
 
 
