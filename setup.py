0c0ac35ea1f9dcd4f9d72e85b3f1da5a10a7e433
Willi Ballenthin <willi.ballenthin@gmail.com>
2011-07-18 18:56:11 -0400

    fix version info in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-registry',
-      version='0.1.1',
+      version='0.2.0',
       description='Read access to Windows Registry files.',
       author='Willi Ballenthin',
       author_email='willi.ballenthin@gmail.com',
