3cd49ca8b9d276254cbc458b3438be5ed344a809
Juan J. Martinez <juan@memset.com>
2012-04-05 15:16:11 +0100

    set the version as a variable

--- Registry/__init__.py
+++ Registry/__init__.py
@@ -15,6 +15,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+_version_ = '0.2.3'
+
 __all__ = [
     'Registry',
     'RegistryParse',
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from Registry import _version_
 
-setup(name='python-registry',
-      version='0.2.3',
+setup(name='registry',
+      version=_version_,
       description='Read access to Windows Registry files.',
       author='Willi Ballenthin',
       author_email='willi.ballenthin@gmail.com',
       url='http://www.williballenthin.com/registry/',
+      license='Apache License (2.0)',
       packages=['Registry'],
      )
+
