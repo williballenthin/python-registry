bd0c4ca1aa22398da3754e4c7e333d0e3b76184c
Willi Ballenthin <william.ballenthin@mandiant.com>
2013-03-21 15:04:30 -0400

    update sample scripts for more usefulness

--- samples/findkey.py
+++ samples/findkey.py
@@ -24,59 +24,77 @@
 #
 
 import sys
+
+import argparse
 from Registry import Registry
 
-paths = []
-value_names = []
-values = []
 
-def rec(key, depth, needle):
-    for value in key.values():
-        if needle in value.name():
-            value_names.append((key.path(), value.name()))
-            sys.stdout.write("n")
-            sys.stdout.flush()
-        try:
-            if needle in str(value.value()):
-                values.append((key.path(), value.name()))
-                sys.stdout.write("v")
+def main():
+    parser = argparse.ArgumentParser(
+        description="Search for a string in a Windows Registry hive")
+    parser.add_argument("registry_hive", type=str,
+                        help="Path to the Windows Registry hive to process")
+    parser.add_argument("query", type=str,
+                        help="Query for which to search")
+    parser.add_argument("-i", action="store_true", dest="case_insensitive",
+                        help="Query for which to search")
+    args = parser.parse_args()
+
+    paths = []
+    value_names = []
+    values = []
+
+
+    def rec(key, depth, needle):
+        for value in key.values():
+            if (args.case_insensitive and needle in value.name().lower()) or needle in value.name():
+                value_names.append((key.path(), value.name()))
+                sys.stdout.write("n")
                 sys.stdout.flush()
-        except UnicodeEncodeError:
-            pass
-            
-    for subkey in key.subkeys():
-        if needle in subkey.name():
-            paths.append(subkey.path())
-            sys.stdout.write("p")
-            sys.stdout.flush()
-        rec(subkey, depth + 1, needle)
+            try:
+                if (args.case_insensitive and needle in str(value.value()).lower()) or needle in str(value.value()):
+                    values.append((key.path(), value.name()))
+                    sys.stdout.write("v")
+                    sys.stdout.flush()
+            except UnicodeEncodeError:
+                pass
+
+        for subkey in key.subkeys():
+            if needle in subkey.name():
+                paths.append(subkey.path())
+                sys.stdout.write("p")
+                sys.stdout.flush()
+            rec(subkey, depth + 1, needle)
+
+    reg = Registry.Registry(args.registry_hive)
+    needle = args.query
+    if args.case_insensitive:
+        needle = needle.lower()
+
+    rec(reg.root(), 0, needle)
+    print ""
+
+    print "[Paths]"
+    for path in paths:
+        print "  - %s" % (path)
+    if len(paths) == 0:
+        print "  (none)"
+    print ""
+
+    print "[Value Names]"
+    for pair in value_names:
+        print "  - %s : %s" % (pair[0], pair[1])
+    if len(value_names) == 0:
+        print "  (none)"
+    print ""
+
+    print "[Values]"
+    for pair in values:
+        print "  - %s : %s" % (pair[0], pair[1])
+    if len(values) == 0:
+        print "  (none)"
+    print ""
 
 
-reg = Registry.Registry(sys.argv[1])
-if len(sys.argv) == 3:
-    needle = sys.argv[2]
-else:
-    needle = ""
-rec(reg.root(), 0, needle)
-print ""
-
-print "[Paths]"
-for path in paths:
-    print "  - %s" % (path)
-if len(paths) == 0:
-    print "  (none)"
-print ""
-
-print "[Value Names]"
-for pair in value_names:
-    print "  - %s : %s" % (pair[0], pair[1])
-if len(value_names) == 0:
-    print "  (none)"
-print ""
-
-print "[Values]"
-for pair in values:
-    print "  - %s : %s" % (pair[0], pair[1])
-if len(values) == 0:
-    print "  (none)"
-print ""
+if __name__ == "__main__":
+    main()
--- samples/timeline.py
+++ samples/timeline.py
@@ -19,32 +19,37 @@ def main():
         description="Timeline Windows Registry key timestamps")
     parser.add_argument("--bodyfile", action="store_true",
                         help="Output in the Bodyfile 3 format")
-    parser.add_argument("registry_hive", type=str,
+    parser.add_argument("registry_hives", type=str, nargs="+",
                         help="Path to the Windows Registry hive to process")
     args = parser.parse_args()
-    items = []
 
-    def rec(key):
+    def rec(key, visitor):
         try:
-            items.append((key.timestamp(), key.path()))
+            visitor(key.timestamp(), key.path())
         except ValueError:
             pass
         for subkey in key.subkeys():
-            rec(subkey)
+            rec(subkey, visitor)
 
-    for path in args.registry_hive:
-        reg = Registry.Registry(args.registry_hive)
-        rec(reg.root())
+    for path in args.registry_hives:
+        reg = Registry.Registry(path)
 
-    if args.bodyfile:
-        hive = guess_hive_name(args.registry_hive)
-        for timestamp, path in items:
-            print u"0|[Registry %s] %s|0|0|0|0|0|%s|0|0|0" % \
-                (hive, path, int(calendar.timegm(timestamp.timetuple())))
+        if args.bodyfile:
+            hive = guess_hive_name(path)
 
-    else:
-        for i in sorted(items, key=lambda x: x[0]):
-            print "%s\t%s" % i
+            def visitor(timestamp, path):
+                try:
+                    print u"0|[Registry %s] %s|0|0|0|0|0|%s|0|0|0" % \
+                      (hive, path, int(calendar.timegm(timestamp.timetuple())))
+                except UnicodeDecodeError:
+                    pass
+
+            rec(reg.root(), visitor)
+        else:
+            items = []
+            rec(reg.root(), lambda a, b: items.append((a, b)))
+            for i in sorted(items, key=lambda x: x[0]):
+                print "%s\t%s" % i
 
 if __name__ == "__main__":
     main()
