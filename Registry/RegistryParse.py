williballenthin/python-registry

commit b95ab27f1b976111f2987c2e77b0b20195160c23

Fix bug when parsing REG_SZ values that contain slack binary data beyond the end of the Unicode string
Fixes #22.

Registry/RegistryParse.py
@@ -690,17 +690,22 @@ class VKRecord(Record):
                 d = HBINCell(self._buf, data_offset, self)
                 s = struct.unpack_from("<%ds" % (data_length), self._buf, d.data_offset())[0]
 
-            try:
-                s = s.decode("utf16").encode("utf8").decode("utf8") # iron out the kinks by
-            except UnicodeDecodeError:                              # converting to and back to a Python str
-                try:
-                    s = s.decode("utf8").encode("utf8").decode("utf8")
-                except UnicodeDecodeError:
-                    try:
-                        s = s.decode("utf8", "replace").encode("utf8").decode("utf8")
-                    except:
-                        print "Well at this point you are screwed."
-                        raise
+            index = s.index("\x00\x00")
+            if index > 2:
+                if s[index - 2] != "\x00":
+                    #  61 00 62 00 63 64 00 00
+                    #                    ^  ^-- end of string
+                    #                    +-- index
+                    s = s[:index + 2]
+                else:
+                    #  61 00 62 00 63 00 00 00
+                    #                 ^     ^-- end of string
+                    #                 +-- index
+                    #  TODO(wb): really, we should check that s[index + 2] == \x00
+                    #    but i think we won't, since the Unicode decode below will fail
+                    s = s[:index + 3]
+
+            s = s.decode("utf16")
             s = s.partition('\x00')[0]
             return s
         elif data_type == RegBin or data_type == RegNone:

commit cd22f80333593ec1efb686c4061b1f5e56baacea

fix bug parsing REG_SZ values with no NULL terminator introduced in b95ab27f1b976111f2987c2e77b0b20195160c23

Registry/RegistryParse.py
@@ -690,20 +690,21 @@ class VKRecord(Record):
                 d = HBINCell(self._buf, data_offset, self)
                 s = struct.unpack_from("<%ds" % (data_length), self._buf, d.data_offset())[0]
 
-            index = s.index("\x00\x00")
-            if index > 2:
-                if s[index - 2] != "\x00":
-                    #  61 00 62 00 63 64 00 00
-                    #                    ^  ^-- end of string
-                    #                    +-- index
-                    s = s[:index + 2]
-                else:
-                    #  61 00 62 00 63 00 00 00
-                    #                 ^     ^-- end of string
-                    #                 +-- index
-                    #  TODO(wb): really, we should check that s[index + 2] == \x00
-                    #    but i think we won't, since the Unicode decode below will fail
-                    s = s[:index + 3]
+            if "\x00\x00" in s:
+                index = s.index("\x00\x00")
+                if index > 2:
+                    if s[index - 2] != "\x00":
+                        #  61 00 62 00 63 64 00 00
+                        #                    ^  ^-- end of string
+                        #                    +-- index
+                        s = s[:index + 2]
+                    else:
+                        #  61 00 62 00 63 00 00 00
+                        #                 ^     ^-- end of string
+                        #                 +-- index
+                        #  TODO(wb): really, we should check that s[index + 2] == \x00
+                        #    but i think we won't, since the Unicode decode below will fail
+                        s = s[:index + 3]
 
             s = s.decode("utf16")
             s = s.partition('\x00')[0]