90343f017dfb71d52faa0cf1d77749f27245204d
Willi Ballenthin <willi.ballenthin@gmail.com>
2014-04-30 11:16:29 -0400

    doc: prep for 1.0.2

--- Registry/__init__.py
+++ Registry/__init__.py
@@ -15,7 +15,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-_version_ = '1.0.0'
+_version_ = '1.0.2'
 
 __all__ = [
     'Registry',
