7b51cd1e4ab1cbc5bb77ecf4718f7db4e5728988
Willi Ballenthin <willi.ballenthin@gmail.com>
2014-04-30 11:26:45 -0400

    doc: prep for tag v1.0.3

--- Registry/__init__.py
+++ Registry/__init__.py
@@ -15,7 +15,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-_version_ = '1.0.2'
+_version_ = '1.0.3'
 
 __all__ = [
     'Registry',
