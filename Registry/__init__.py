8fe11a6f534a91d09f32d8191f2229797781a0c3
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-12-22 10:29:09 -0500

    bump version 1.2.0

--- Registry/__init__.py
+++ Registry/__init__.py
@@ -15,7 +15,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-_version_ = '1.1.0'
+_version_ = '1.2.0'
 
 __all__ = [
     'Registry',
